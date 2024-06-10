7a38531d5bc44841f0cadd8ed60c72025894f989
igurdeep13 <g.s.basantpuria@gmail.com>
2024-06-10 12:27:09 -0400

    Add code

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const searchBox = document.querySelector(".searchBox");
 const searchBtn = document.querySelector(".searchBtn");
 const recipeContainer = document.querySelector(".recipe-container");
+const recipeDetailsContent = document.querySelector(".recipe-details-content");
+const recipeCloseBtn = document.querySelector(".recipe-closeBtn");
+
+
+
 
 // Functions to get Recipes...
 const fetchRecipes = async (query) => {
@@ -35,8 +40,36 @@ const fetchRecipes = async (query) => {
 
 };
 
+
+// Function to fetch ingredients and measurements
+const fetchIngredients = (meal) => {
+  let ingredientsList = "";
+  for(let i=1; i<=20; i++){
+    const ingredient = meal[`strIngredients${i}`];
+    if(ingredient){
+      const measure = meal[`strMeasure${i}`];
+      ingredientsList += `<li>${measure} ${ingredient}</li>`
+    }
+    else{ 
+      break;
+    }
+  }
+  return ingredientsList
+}
+
+
+const openRecipePopup = (meal) => {
+  recipeDetailsContent.innerHTML = `
+  <h2>${meal.strMeal}</h2>
+  <h3>Ingredients:</h3>
+  <ul>${fetchIngredients(meal)}</ul>
+  `
+  recipeDetailsContent.parentElement.style.display = 'block'
+}
+
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const searchInput = searchBox.value.trim();
   fetchRecipes(searchInput);
+
 });
